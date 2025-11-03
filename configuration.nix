d52578381a54f864b63e9112e2a678f8183ce69f
EineNachtImMai <titouan.cazaubieilh@enseirb-matmeca.fr>
2025-11-03 16:09:50 +0100

    just cleanup

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,6 @@
 }: {
   imports = [
     ./configs
-    # inputs.hyprland.nixosModules.default
   ];
 
   catppuccin = {
@@ -43,11 +42,5 @@
     NIXOS_OZONE_WL = "1";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05";
 }
